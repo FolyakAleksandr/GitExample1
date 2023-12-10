c302deefeb88f72e9716b9097422ebe8af77e8be
Aleksandr <a.folyak2000@gmail.com>
2023-12-10 17:34:51 +0600

    add new label

--- GitTest1/ViewController.swift
+++ GitTest1/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Aleksandr"
+        label.frame = view.frame
     }
 
 
